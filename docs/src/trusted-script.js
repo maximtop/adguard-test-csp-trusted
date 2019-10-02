933f93cc9e103e5e55e8980b801725e96df9baf4
Maxim Topchu <maximtop@gmail.com>
2019-10-02 18:30:17 +0300

    fix url

--- docs/src/trusted-script.js
+++ docs/src/trusted-script.js
@@ -1,6 +1,7 @@
 (async () => {
-    const response = await fetch('https://maximtop.github.io/adguard-test-csp-trusted/src/info.json');
+    const response = await fetch('src/info.json');
     const data = await response.json();
+    console.log(data);
     const jsBlock = document.querySelector('.test-js .text');
     jsBlock.innerText = data.message;
 })();
